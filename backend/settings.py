37d0d8d2005a71835dcab1733b0e987b85f9ed47
Ajas333 <ajaskm333@gmail.com>
2024-07-03 16:12:10 +0530

    host

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://13.49.238.14/"]
 
 
 # Application definition
